b4516ecc14bb7c7bb6443067b80aa546fee0f2b4
Torkel Ödegaard <torkel@grafana.org>
2016-10-02 21:47:04 +0200

    more work on minification and filerev

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
 module.exports = function(grunt) {
 
-  require('load-grunt-tasks')(grunt);
+  require("load-grunt-tasks")(grunt);
 
-  grunt.loadNpmTasks('grunt-execute');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks("grunt-execute");
+  grunt.loadNpmTasks("grunt-contrib-clean");
 
   grunt.initConfig({
     clean: ["dist"],
@@ -16,20 +16,20 @@ module.exports = function(grunt) {
         livereload: 3003,
       },
       content: {
-        files: ['content/**/*', 'layouts/**/*'],
-        tasks: ['hugo'],
+        files: ["content/**/*", "layouts/**/*"],
+        tasks: ["hugo"],
         options: {spawn: false},
       },
       sass: {
-        files: ['static/sass/**/*'],
-        tasks: ['sass', 'postcss'],
+        files: ["static/sass/**/*"],
+        tasks: ["sass", "postcss"],
         options: {
           spawn: false
         }
       },
       js: {
-        files: ['static/js/**/*'],
-        tasks: ['babel', 'systemjs'],
+        files: ["static/js/**/*"],
+        tasks: ["babel", "systemjs"],
         options: {
           spawn: false
         }
@@ -51,19 +51,21 @@ module.exports = function(grunt) {
       options: {
         map: true,
         processors: [
-          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+          require("autoprefixer")({browsers: "last 2 versions"}), // add vendor prefixes
         ]
       },
-      dist: {src: 'dist/css/*.css'}
+      dist: {
+        src: "dist/css/*.css"
+      }
     },
 
     connect: {
       mysite: {
         options: {
-          hostname: '127.0.0.1',
+          hostname: "127.0.0.1",
           port: 3002,
-          protocol: 'http',
-          base: 'dist',
+          protocol: "http",
+          base: "dist",
           livereload: 3003,
           keepalive: true,
         }
@@ -74,15 +76,15 @@ module.exports = function(grunt) {
       options: {
         sourceMap: true,
         presets:  ["es2015"],
-        plugins: ['transform-es2015-modules-systemjs', "transform-es2015-for-of"],
+        plugins: ["transform-es2015-modules-systemjs", "transform-es2015-for-of"],
       },
       dist: {
         files: [{
-          cwd: 'static',
+          cwd: "static",
           expand: true,
-          src: ['js/**/*.js', '!js/system.conf.js'],
-          dest: 'dist',
-          ext:'.js'
+          src: ["js/**/*.js", "!js/system.conf.js"],
+          dest: "dist",
+          ext:".js"
         }]
       },
     },
@@ -92,41 +94,85 @@ module.exports = function(grunt) {
         sfx: true,
         baseURL: "./dist",
         configFile: "./dist/js/system.conf.js",
-        minify: true,
+        minify: false,
         build: {mangle: false}
-       },
-        dist: {
-          files: [{
+      },
+      dist: {
+        files: [{
           "src":  "./dist/js/app.js",
           "dest": "./dist/js/app.min.js"
-          }]
+        }]
+      }
+    },
+
+    cssmin: {
+      build: {
+        expand: true,
+        cwd: "dist/css",
+        src: ["*.css"],
+        dest: "dist/css"
+      }
+    },
+
+    uglify: {
+      build: {
+        expand: true,
+        src: ["**/*.js"],
+        dest: 'dist/js',
+        cwd: 'dist/js',
+        options: {
+          quite: true,
+          compress: {},
+          preserveComments: false,
         }
-     }
+      }
+    },
+
+    usemin: {
+      html: ["dist/**/*.html"]
+    },
+
+    filerev: {
+      options: {encoding: "utf8", algorithm: "md5", length: 8},
+      js: {
+        src: "dist/js/app.js",
+        dest: "dist/js",
+      }
+    }
+
   });
 
-  grunt.registerTask('hugo', function(target) {
+  grunt.registerTask("hugo", function(target) {
     var done = this.async();
     var args = ["--destination=dist/"];
 
-    if (target === 'dev') {
-      args.push('--baseUrl=http://localhost:1345');
-      args.push('--buildDrafts=true');
-      args.push('--buildFuture=true');
+    if (target === "dev") {
+      args.push("--baseUrl=http://localhost:1345");
+      args.push("--buildDrafts=true");
+      args.push("--buildFuture=true");
     }
 
-    hugo = require('child_process').spawn('hugo', args, {stdio: 'inherit'});
-    hugo.on('exit', function() { done(true); });
-    hugo.on('error', function() { done(true); });
+    hugo = require("child_process").spawn("hugo", args, {stdio: "inherit"});
+    hugo.on("exit", function() { done(true); });
+    hugo.on("error", function() { done(true); });
   });
 
-  grunt.registerTask('default', [
-    'clean',
-    'hugo:dev',
-    'sass',
-    'postcss',
-    'babel',
-    'systemjs',
-    ]);
+  grunt.registerTask("default", [
+      "hugo:dev",
+      "sass",
+      "postcss",
+      "babel",
+      "systemjs",
+  ]);
+
+  grunt.registerTask("build", [
+      "clean",
+      "default",
+      "cssmin",
+      "uglify",
+      "filerev",
+      "usemin",
+  ]);
 
 };
 
